9ac4559d639e670a2a305ccb43933c7fe1789aaf
dcfernandez1023 <54515508+dcfernandez1023@users.noreply.github.com>
2020-08-12 18:28:03 -0700

    commit

--- TdApis.py
+++ TdApis.py
@@ -10,7 +10,7 @@ import flask_apis.User as User
 
 
 ##GLOBAL VARIABLES
-app = flask.Flask(__name__, static_folder = "../td-client/build", static_url_path = "/")
+app = flask.Flask("__main__", static_folder = "../td-client/build", static_url_path = "/")
 #app = flask.Flask("__main__")
 app.config['SECRET_KEY'] = 'Th1s1ss3cr3t'
 TEMP_USER = "TEMP"
@@ -119,5 +119,5 @@ def post_data(username):
 
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0") #debug = False, port = os.environ.get("PORT", 80))
+    app.run(host = "0.0.0.0", debug = False, port = os.environ.get("PORT", 80))
 #app.run(host = "localhost", port = 8080, debug = True)
